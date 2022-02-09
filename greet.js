99a41e59b77780be4120cf7da1d16edad6d8f391
Attiya <adarensburg@live.com>
2022-02-09 12:31:36 -0600

    greet.js completed

--- greet.js
+++ greet.js
@@ -6,5 +6,25 @@ function say(name) {
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
-say(helloMessage, "John");
-say(heyThereMessage, "Michael");
+// let john = 'John'
+// let michael = 'Michael'
+
+// class Person{
+//   constructor(name){
+//     this.name = name
+//   }
+
+// }
+
+let message1 = helloMessage
+let message2 = heyThereMessage
+
+let john = 'John';
+say.call(message1, john);
+
+let michael = 'Michael';
+say.call(message2, michael);
+
+// say(heyThereMessage, "Michael");
+
+// john.say('john')
